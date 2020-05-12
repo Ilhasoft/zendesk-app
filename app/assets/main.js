7517d0d3cd6be00f2741ad9b32f71cefc5b9fd8c
Rowan Seymour <rowanseymour@gmail.com>
2020-05-12 17:27:07 -0500

    WIP

--- app/assets/main.js
+++ app/assets/main.js
@@ -1,9 +1,9 @@
-const domain = "$DOMAIN$";
-const pathExternalSrc = "ticket.via.source.from.service_info.registered_integration_service_external_id";
-const pathRequesterIdentity = "ticket.requester.identities.0";
+var domain = "$DOMAIN$";
+var pathExternalSrc = "ticket.via.source.from.service_info.registered_integration_service_external_id";
+var pathRequesterIdentity = "ticket.requester.identities.0";
 
 (function () {
-    const app = new Vue({
+    var app = new Vue({
         el: '#app',
         data: {
             ready: false,
@@ -12,14 +12,31 @@ const pathRequesterIdentity = "ticket.requester.identities.0";
         },
     });
 
-    const client = ZAFClient.init();
+    var client = ZAFClient.init();
 
     client.get([pathExternalSrc, pathRequesterIdentity]).then(function (data) {
         app.ourTicket = (data[pathExternalSrc] == domain);
         if (app.ourTicket) {
-            const contactUUID = data[pathRequesterIdentity].value.split(":")[1];
+            var contactUUID = data[pathRequesterIdentity].value.split(":")[1];
             app.contactURL = `https://${domain}/contact/read/${contactUUID}/`;
+
+            client.on('ticket.status.changed', function (e) {
+                if (e == "solved") {
+                    handleTicketSolved();
+                }
+            });
         }
         app.ready = true;
     });
+
+    function handleTicketSolved() {
+        // our external ticket UUID is stored in display_info on comments from us.. like the initial one
+        client.get("ticket.comments").then(function (data) {
+            var comments = data["ticket.comments"];
+            var initialComment = comments[comments.length - 1];
+            var ticketUUID = initialComment.channelDisplayInfo["temba-ticket"]["uuid"];
+
+            console.log(`TODO: notify $BRAND$ that ticket ${ticketUUID} has been closed`);
+        });
+    }
 })();
